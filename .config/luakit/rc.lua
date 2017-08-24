022354dab3bfddba1a006201b732588036e6b374
Marius Orcsik <marius@habarnam.ro>
2017-08-24 22:17:11 +0200

    Updated rc.lua

--- .config/luakit/rc.lua
+++ .config/luakit/rc.lua
@@ -2,31 +2,17 @@
 -- luakit configuration file, more information at https://aidanholm.github.io/luakit/ --
 ----------------------------------------------------------------------------------------
 
-if unique then
-    unique.new("org.luakit")
-    -- Check for a running luakit instance
-    if unique.is_running() then
-        if uris[1] then
-            for _, uri in ipairs(uris) do
-                if lfs.attributes(uri) then uri = os.abspath(uri) end
-                unique.send_message("tabopen " .. uri)
-            end
-        else
-            unique.send_message("winopen")
-        end
-        luakit.quit()
-    end
-end
+require "lfs"
+
+require "unique_instance"
 
 -- Set the number of web processes to use. A value of 0 means 'no limit'.
 luakit.process_limit = 0
+-- Set the cookie storage location
+soup.cookies_storage = luakit.data_dir .. "/cookies.db"
 
 -- Load library of useful functions for luakit
 local lousy = require "lousy"
-
--- Load users global config
--- ("$XDG_CONFIG_HOME/luakit/globals.lua" or "/etc/xdg/luakit/globals.lua")
-require "globals"
 
 -- Load users theme
 -- ("$XDG_CONFIG_HOME/luakit/theme.lua" or "/etc/xdg/luakit/theme.lua")
@@ -41,6 +27,9 @@ local window = require "window"
 -- ("$XDG_CONFIG_HOME/luakit/webview.lua" or "/etc/xdg/luakit/webview.lua")
 local webview = require "webview"
 
+-- Add luakit;//log/ chrome page
+local log_chrome = require "log_chrome"
+
 window.add_signal("build", function (w)
     local widgets, l, r = require "lousy.widget", w.sbar.l, w.sbar.r
 
@@ -51,60 +40,59 @@ window.add_signal("build", function (w)
 
     -- Right-aligned status bar widgets
     r.layout:pack(widgets.buf())
+    r.layout:pack(log_chrome.widget())
     r.layout:pack(widgets.ssl())
     r.layout:pack(widgets.tabi())
     r.layout:pack(widgets.scroll())
 end)
 
--- Load users mode configuration
--- ("$XDG_CONFIG_HOME/luakit/modes.lua" or "/etc/xdg/luakit/modes.lua")
-require "modes"
+-- Load luakit binds and modes
+local modes = require "modes"
+local binds = require "binds"
 
--- Load users keybindings
--- ("$XDG_CONFIG_HOME/luakit/binds.lua" or "/etc/xdg/luakit/binds.lua")
-require "binds"
+local settings = require "settings"
 
 ----------------------------------
 -- Optional user script loading --
 ----------------------------------
 
 -- Add adblock
-require "adblock"
-require "adblock_chrome"
+local adblock = require "adblock"
+local adblock_chrome = require "adblock_chrome"
 
-require "webinspector"
+local webinspector = require "webinspector"
 
 -- Add uzbl-like form filling
-require "formfiller"
+local formfiller = require "formfiller"
 
 -- Add proxy support & manager
-require "proxy"
+local proxy = require "proxy"
 
 -- Add quickmarks support & manager
-require "quickmarks"
+local quickmarks = require "quickmarks"
 
 -- Add session saving/loading support
 local session = require "session"
 
 -- Add command to list closed tabs & bind to open closed tabs
-require "undoclose"
+local undoclose = require "undoclose"
 
 -- Add command to list tab history items
-require "tabhistory"
+local tabhistory = require "tabhistory"
 
 -- Add greasemonkey-like javascript userscript support
-require "userscripts"
+local userscripts = require "userscripts"
 
 -- Add bookmarks support
-require "bookmarks"
-require "bookmarks_chrome"
+local bookmarks = require "bookmarks"
+local bookmarks_chrome = require "bookmarks_chrome"
 
 -- Add download support
 local downloads = require "downloads"
-require "downloads_chrome"
+local downloads_chrome = require "downloads_chrome"
 
 -- Add automatic PDF downloading and opening
-require "viewpdf"
+local viewpdf = require "viewpdf"
 
 -- Example using xdg-open for opening downloads / showing download folders
 downloads.add_signal("open-file", function (file)
@@ -113,72 +101,64 @@ downloads.add_signal("open-file", function (file)
 end)
 
 -- Add vimperator-like link hinting & following
-require "follow"
+local follow = require "follow"
 
 -- Add command history
-require "cmdhist"
+local cmdhist = require "cmdhist"
 
 -- Add search mode & binds
-require "search"
+local search = require "search"
 
 -- Add ordering of new tabs
-require "taborder"
+local taborder = require "taborder"
 
 -- Save web history
-require "history"
-require "history_chrome"
+local history = require "history"
+local history_chrome = require "history_chrome"
 
-require "help_chrome"
-require "introspector_chrome"
+local help_chrome = require "help_chrome"
+local introspector_chrome = require "introspector_chrome"
 
 -- Add command completion
-require "completion"
+local completion = require "completion"
 
 -- Press Control-E while in insert mode to edit the contents of the currently
 -- focused <textarea> or <input> element, using `xdg-open`
-require "open_editor"
+local open_editor = require "open_editor"
 
 -- NoScript plugin, toggle scripts and or plugins on a per-domain basis.
 -- `,ts` to toggle scripts, `,tp` to toggle plugins, `,tr` to reset.
--- Remove all "enable_scripts" & "enable_plugins" lines from your
--- domain_props table (in config/globals.lua) as this module will conflict.
+-- If you use this module, don't use any site-specific `enable_scripts` or
+-- `enable_plugins` settings, as these will conflict.
 --require "noscript"
 
-require "follow_selected"
-require "go_input"
-require "go_next_prev"
-require "go_up"
-
--- Block insecure content on secure pages by default
--- Add a bind to w:toggle_mixed_content() to temporarily enable mixed content
--- for the current tab.
-require "mixed_content"
+local follow_selected = require "follow_selected"
+local go_input = require "go_input"
+local go_next_prev = require "go_next_prev"
+local go_up = require "go_up"
 
 -- Filter Referer HTTP header if page domain does not match Referer domain
 require_web_module("referer_control_wm")
 
-require "error_page"
+local error_page = require "error_page"
 
 -- Add userstyles loader
-require "styles"
+local styles = require "styles"
 
 -- Hide scrollbars on all pages
-require "hide_scrollbars"
-
--- Automatically apply per-domain webview properties
-require "domain_props"
+local hide_scrollbars = require "hide_scrollbars"
 
 -- Add a stylesheet when showing images
-require "image_css"
+local image_css = require "image_css"
 
 -- Add a new tab page
-require "newtab_chrome"
+local newtab_chrome = require "newtab_chrome"
 
 -- Add tab favicons mod
-require "tab_favicons"
+local tab_favicons = require "tab_favicons"
 
 -- Add :view-source command
-require "view_source"
+local view_source = require "view_source"
 
 -----------------------------
 -- End user script loading --
@@ -193,24 +173,6 @@ if w then
 else
     -- Or open new window
     window.new(uris)
-end
-
--------------------------------------------
--- Open URIs from other luakit instances --
--------------------------------------------
-
-if unique then
-    unique.add_signal("message", function (msg, screen)
-        local cmd, arg = string.match(msg, "^(%S+)%s*(.*)")
-        local ww = lousy.util.table.values(window.bywidget)[1]
-        if cmd == "tabopen" then
-            ww:new_tab(arg)
-        elseif cmd == "winopen" then
-            ww = window.new((arg ~= "") and { arg } or {})
-        end
-        ww.win.screen = screen
-        ww.win.urgency_hint = true
-    end)
 end
 
 -- vim: et:sw=4:ts=8:sts=4:tw=80
