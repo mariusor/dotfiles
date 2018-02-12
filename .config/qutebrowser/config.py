9cfa5465fb8dad464353d7cc8b21a6399603d377
Marius Orcsik <marius@habarnam.ro>
2018-02-12 22:07:23 +0100

    Adding qutebrowser config

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,42 @@
+c.colors.tabs.bar.bg: '#555555'
+c.colors.tabs.even.bg: '#666666'
+c.colors.tabs.even.fg: 'white'
+c.colors.tabs.odd.bg: '#666666'
+c.colors.tabs.odd.fg: 'white'
+c.colors.tabs.selected.even.bg: '#006699'
+c.colors.tabs.selected.even.fg: 'white'
+c.colors.tabs.selected.odd.bg: '#006699'
+c.colors.tabs.selected.odd.fg: 'white'
+c.colors.webpage.bg: 'white'
+c.completion.use_best_match: True
+c.content.default_encoding: 'utf-8'
+c.content.windowed_fullscreen: False
+c.downloads.location.directory: '~/Downloads'
+c.downloads.location.prompt: True
+c.editor.command: [ 'vim', '{file}', '+{line}' ]
+c.fonts.completion.category: 'bold 12pt monospace'
+c.fonts.completion.entry: '12pt monospace'
+c.fonts.debug_console: '12pt monospace'
+c.fonts.downloads: '10pt monospace'
+c.fonts.hints: 'bold 12pt monospace'
+c.fonts.keyhint: '12pt monospace'
+c.fonts.messages.error: '12pt monospace'
+c.fonts.messages.info: '10pt monospace'
+c.fonts.messages.warning: '10pt monospace'
+c.fonts.prompts: '10pt sans-serif'
+c.fonts.statusbar: '12pt monospace'
+c.fonts.tabs: '12pt monospace'
+c.fonts.web.family.fixed: null
+c.fonts.web.family.serif: null
+c.fonts.web.family.standard: 'sans'
+c.fonts.web.size.default: 18
+c.fonts.web.size.default_fixed: 16
+c.fonts.web.size.minimum: 12
+c.hints.mode: 'number'
+c.input.insert_mode.auto_leave: True
+c.input.insert_mode.auto_load: True
+c.tabs.indicator.padding: { 'bottom': 2, 'left': 0, 'right': 4, 'top': 2}
+c.tabs.indicator.width: 5
+c.tabs.padding: { 'bottom': 2, 'left': 5, 'right': 5, 'top': 1}
+c.tabs.title.format_pinned: '{index} {host}'
+c.tabs.width: '20%'
