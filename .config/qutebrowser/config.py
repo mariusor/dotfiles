9970b6766965556aaac6074d991fdeb068610de6
Marius Orcsik <marius@habarnam.ro>
2018-02-14 15:30:25 +0100

    Updated the configuration to use the config.set instead of c object

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,42 +1,54 @@
-c.colors.tabs.bar.bg: '#555555'
-c.colors.tabs.even.bg: '#666666'
-c.colors.tabs.even.fg: 'white'
-c.colors.tabs.odd.bg: '#666666'
-c.colors.tabs.odd.fg: 'white'
-c.colors.tabs.selected.even.bg: '#006699'
-c.colors.tabs.selected.even.fg: 'white'
-c.colors.tabs.selected.odd.bg: '#006699'
-c.colors.tabs.selected.odd.fg: 'white'
-c.colors.webpage.bg: 'white'
-c.completion.use_best_match: True
-c.content.default_encoding: 'utf-8'
-c.content.windowed_fullscreen: False
-c.downloads.location.directory: '~/Downloads'
-c.downloads.location.prompt: True
-c.editor.command: [ 'vim', '{file}', '+{line}' ]
-c.fonts.completion.category: 'bold 12pt monospace'
-c.fonts.completion.entry: '12pt monospace'
-c.fonts.debug_console: '12pt monospace'
-c.fonts.downloads: '10pt monospace'
-c.fonts.hints: 'bold 12pt monospace'
-c.fonts.keyhint: '12pt monospace'
-c.fonts.messages.error: '12pt monospace'
-c.fonts.messages.info: '10pt monospace'
-c.fonts.messages.warning: '10pt monospace'
-c.fonts.prompts: '10pt sans-serif'
-c.fonts.statusbar: '12pt monospace'
-c.fonts.tabs: '12pt monospace'
-c.fonts.web.family.fixed: null
-c.fonts.web.family.serif: null
-c.fonts.web.family.standard: 'sans'
-c.fonts.web.size.default: 18
-c.fonts.web.size.default_fixed: 16
-c.fonts.web.size.minimum: 12
-c.hints.mode: 'number'
-c.input.insert_mode.auto_leave: True
-c.input.insert_mode.auto_load: True
-c.tabs.indicator.padding: { 'bottom': 2, 'left': 0, 'right': 4, 'top': 2}
-c.tabs.indicator.width: 5
-c.tabs.padding: { 'bottom': 2, 'left': 5, 'right': 5, 'top': 1}
-c.tabs.title.format_pinned: '{index} {host}'
-c.tabs.width: '20%'
+config.set('colors.tabs.bar.bg', '#111111')
+config.set('colors.tabs.even.bg', '#666666')
+config.set('colors.tabs.even.fg', 'white')
+config.set('colors.tabs.odd.bg', '#666666')
+config.set('colors.tabs.odd.fg', 'white')
+config.set('colors.tabs.selected.even.bg', '#004477')
+config.set('colors.tabs.selected.even.fg', '#ffffff')
+config.set('colors.tabs.selected.odd.bg', '#004477')
+config.set('colors.tabs.selected.odd.fg', '#ffffff')
+config.set('colors.webpage.bg', '#cccccc')
+config.set('completion.use_best_match', True)
+config.set('content.default_encoding', 'utf-8')
+config.set('content.windowed_fullscreen', False)
+config.set('downloads.location.directory', '~/Downloads')
+config.set('downloads.location.prompt', True)
+config.set('editor.command', [ 'nvim-qt', '{file}', '+{line}' ])
+config.set('fonts.completion.category', 'bold 12pt monospace')
+config.set('fonts.completion.entry', '12pt monospace')
+config.set('fonts.debug_console', '12pt monospace')
+config.set('fonts.downloads', '10pt monospace')
+config.set('fonts.hints', 'bold 12pt monospace')
+config.set('fonts.keyhint', '12pt monospace')
+config.set('fonts.messages.error', '12pt monospace')
+config.set('fonts.messages.info', '10pt monospace')
+config.set('fonts.messages.warning', '10pt monospace')
+config.set('fonts.prompts', '10pt sans-serif')
+config.set('fonts.statusbar', '12pt monospace')
+config.set('fonts.tabs', '12pt monospace')
+config.set('fonts.web.family.standard', 'sans')
+config.set('fonts.web.size.default', 18)
+config.set('fonts.web.size.default_fixed', 16)
+config.set('fonts.web.size.minimum', 12)
+config.set('hints.mode', 'number')
+config.set('input.insert_mode.auto_leave', True)
+config.set('input.insert_mode.auto_load', True)
+config.set('tabs.indicator.padding', { 'bottom': 2, 'left': 0, 'right': 4, 'top': 2})
+config.set('tabs.indicator.width', 5)
+config.set('tabs.padding', { 'bottom': 4, 'left': 5, 'right': 5, 'top': 3})
+config.set('tabs.title.format', '{host}{title_sep}{title} [{index}]')
+config.set('tabs.title.format_pinned', '{host} [{index}]')
+config.set('tabs.width', '20%')
+config.bind('t', 'set-cmd-text -s :open -t ')
+config.bind('T', 'set-cmd-text -s :open -t {url}')
+config.bind('O', 'set-cmd-text -s :open {url}')
+config.bind('gt', 'tab-next')
+config.bind('gT', 'tab-prev')
+config.bind(',v', 'spawn mpv {url}')
+config.bind('<Ctrl-o>', 'back')
+config.bind('<Ctrl-i>', 'forward')
+config.unbind('H')
+config.unbind('J')
+config.unbind('K')
+config.unbind('L')
+
