1bcfe8bd396c23e39c277e96e95ea951993b0aaa
marius <marius@habarnam.ro>
2021-09-29 18:08:51 +0200

    Adding new headphones to the connect script

--- .local/bin/connect-headphones.sh
+++ .local/bin/connect-headphones.sh
@@ -1,6 +1,12 @@
-#!/bin/env sh
+#!/bin/env bash
+
+mac=00:1B:66:8B:5F:C2
+what=$(basename ${0})
+if [ ${what} = "connect-shockz.sh" ]; then
+    mac=20:74:CF:D3:64:23
+fi
 rfkill unblock bluetooth
-bluetoothctl disconnect 00:1B:66:8B:5F:C2
+bluetoothctl disconnect ${mac}
 bluetoothctl power on
-bluetoothctl connect 00:1B:66:8B:5F:C2
+bluetoothctl connect ${mac}
 #pactl set-card-profile bluez_card.00_1B_66_8B_5F_C2 a2dp_sink
